d2fda81d5ec59b8fa9949f8bfa38f84c5a0c325c
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-11-24 19:35:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os 
+import os
 import math
+
 
 def list_of_files(directory, extension):
     files_names = []
@@ -8,52 +9,61 @@ def list_of_files(directory, extension):
             files_names.append(filename)
     return sorted(files_names)
 
+
 # Call of the function
 directory = "./speeches"
 files_names = list_of_files(directory, "txt")
 print(files_names)
 print()
 
-#Extraire les noms des présidents à partir des noms des fichiers texte fournis ; 
+
+# Extraire les noms des présidents à partir des noms des fichiers texte fournis ;
 def extract_president_names(file):
     liste = set()
     president_names = set()  # Utilisez un ensemble pour éviter les doublons
     # on fait une première boucle pour mettre dans une liste tous les présidents avec leurs numéros
-    # permettant d'enlever leurs type et + 
+    # permettant d'enlever leurs type et +
     for i in files_names:
         president_names.add(i[11:-4])
 
     # on fait 2 boucles pour accéder à chaque caractère et ainsi supprimer les doublons
     for i in president_names:
-        # on va créer une liste où on traitera chaque president à part, en utilisant leur table ascii pour 
+        # on va créer une liste où on traitera chaque president à part, en utilisant leur table ascii pour
         # déterminer leur type
         vrai_prenom = []
         for j in range(len(i)):
 
             # on vérifie la condition qui dit que le caractère n'est pas un nombre
             if ord(i[j]) < 48 or ord(i[j]) > 57:
-               continue
-            else: 
+                continue
+            else:
                 vrai_prenom.append(i[:j])
-        
+
         # si elle ne possède pas de numéro, on l'ajoute dans liste pour ensuite être copier dans le set
         if vrai_prenom == []:
             vrai_prenom.append(i)
-        liste.add(vrai_prenom[0]) #pour changer la liste en mot      
+        liste.add(vrai_prenom[0])  # pour changer la liste en mot
     return liste
 
+
 print(extract_president_names(files_names))
 
-#Associer à chaque président un prénom 
-dico = {"Giscard dEstaing" :"Valéry", "Macron" : "Emmanuelle" , "Mitterrand" : "François" , "Sarkozy" : "Nicolas" , "Chirac" : "Jacques" , "Hollande" : "François" }
-def president_prenom(x) : 
-    return(dico[x])
+# Associer à chaque président un prénom
+dico = {"Giscard dEstaing": "Valéry", "Macron": "Emmanuelle", "Mitterrand": "François", "Sarkozy": "Nicolas",
+        "Chirac": "Jacques", "Hollande": "François"}
+
+
+def president_prenom(x):
+    return (dico[x])
+
+
 print(president_prenom("Macron"))
 
-# Afficher la liste des noms des présidents (attention aux doublons) ; 
+# Afficher la liste des noms des présidents (attention aux doublons) ;
 print(extract_president_names(files_names))
 
-#convertir texte en miniscules + creation repertoire cleaned + copier contenue dans le nouveau repertoire
+
+# convertir texte en miniscules + creation repertoire cleaned + copier contenue dans le nouveau repertoire
 def convertir_textes_miniscules():
     # on trouve le chemin pour acceder à speeches et ensuite on obtient la liste files.names
     directory = "./speeches"
@@ -63,48 +73,51 @@ def convertir_textes_miniscules():
     directory_cleaned = "./cleaned"
 
     # Vérifiez si le répertoire n'existe pas déjà, puis créez-le
-    #if not os.path.exists(directory_cleaned):
+    # if not os.path.exists(directory_cleaned):
     os.makedirs(directory_cleaned, exist_ok=True)
-        
-    #On va acceder au nom des fichiers pour trouver leurs chemins
-    for i in files_names:
 
+    # On va acceder au nom des fichiers pour trouver leurs chemins
+    for i in files_names:
         # On crée le chemin et le noveau nom du fichier copier
-        chemin_cleaned = directory_cleaned+"/"+i
+        chemin_cleaned = directory_cleaned + "/" + i
         chemin_cleaned = directory_cleaned + "/" + i
 
         # On crée le fihcier copier, puis on copie le contenu du premier fihcier et on le rajoute le tout en miniscule
         with open(chemin_cleaned, "w") as fichier:
-            fichier2 = open(directory + "/" + i,"r")
+            fichier2 = open(directory + "/" + i, "r")
             contenu = fichier2.read()
             fichier.write(contenu.lower())
-            
+
+
 def supprimer_ponctuation():
-    #trouve le chemin d'accés des fichiers à nettoyer
+    # trouve le chemin d'accés des fichiers à nettoyer
     directory = "./cleaned"
     files_names = list_of_files(directory, "txt")
-    #on fait une boucle qui ouvre chaque fichier du répertoire un par un et on met le texte dans une variable
+    # on fait une boucle qui ouvre chaque fichier du répertoire un par un et on met le texte dans une variable
     for nom_fichier in files_names:
         with open(directory + "/" + nom_fichier, "r") as fichier:
             contenu = fichier.read()
         indice = []
-        #dans une liste on met toutes les positions où il y a un signe de ponctuations
-        for i in range (len(contenu)):
-            if (ord(contenu[i]) >= 21 and ord(contenu[i]) <=47) or (ord(contenu[i]) >=58 and ord(contenu[i])<= 64) or (contenu[i])=="\n" :
+        # dans une liste on met toutes les positions où il y a un signe de ponctuations
+        for i in range(len(contenu)):
+            if (ord(contenu[i]) >= 21 and ord(contenu[i]) <= 47) or (
+                    ord(contenu[i]) >= 58 and ord(contenu[i]) <= 64) or (contenu[i]) == "\n":
                 indice.append(i)
         n = 0
         contenu1 = ""
-        # on fait une boucle qui va copier le contenue du texte jusqu'à la postion d'une ponctuation et remplace cette 
-        #ponctuation par un espace et cela jusqu'à la fin du texte
-        for i in indice :
+        # on fait une boucle qui va copier le contenue du texte jusqu'à la postion d'une ponctuation et remplace cette
+        # ponctuation par un espace et cela jusqu'à la fin du texte
+        for i in indice:
             contenu1 = contenu1 + contenu[n:i] + " "
-            n = i+1
-        #reécrit le nouveau texte sans la ponctuation
+            n = i + 1
+        # reécrit le nouveau texte sans la ponctuation
         with open(directory + "/" + nom_fichier, "w") as fichier_modifie:
             fichier_modifie.write(contenu1)
-        
+
+
 print(convertir_textes_miniscules())
 print(supprimer_ponctuation())
+
 
 # Écrire une fonction qui prend en paramètre une chaine de caractères et qui retourne un dictionnaire
 # associant à chaque mot le nombre de fois qu’il apparait dans la chaine de caractères
@@ -113,11 +126,12 @@ def occurences_chaines(chaine):
     dico = {}
     liste_mot = []
 
-    # On fait une boucle pour obtenir tous les indices où se trouvent de signes de ponctuation 
+    # On fait une boucle pour obtenir tous les indices où se trouvent de signes de ponctuation
     for i in range(len(chaine)):
-        if (ord(chaine[i]) >= 21 and ord(chaine[i]) <=47) or (ord(chaine[i]) >=58 and ord(chaine[i])<= 64) or (chaine[i])=="\n" :
+        if (ord(chaine[i]) >= 21 and ord(chaine[i]) <= 47) or (ord(chaine[i]) >= 58 and ord(chaine[i]) <= 64) or (
+        chaine[i]) == "\n":
             indice.append(i)
-        
+
     # On utlisera les indices precedemment acquis pour effectuer des slides dans la chaine de caractere initial et
     # ainsi copier les mots.
     n = 0
@@ -134,34 +148,34 @@ def occurences_chaines(chaine):
         dico[i] = occurences
     return dico
 
-# print(occurences_chaines("Écrire une fonction qui prend en paramètre une chaine de caractères 
-# et qui retourne un dictionnaire associant à chaque mot le nombre de fois qu’il apparait dans 
+
+# print(occurences_chaines("Écrire une fonction qui prend en paramètre une chaine de caractères
+# et qui retourne un dictionnaire associant à chaque mot le nombre de fois qu’il apparait dans
 # la chaine de caractères."))
 
 # Écrire une fonction qui prend en paramètre le répertoire où se trouve l’ensemble des fichiers du corpus
-# et qui retourne un dictionnaire associant à chaque mot son score IDF. 
+# et qui retourne un dictionnaire associant à chaque mot son score IDF.
 
 def IDF_par_fichier(repertoire):
-
-    directory = './{}'.format(repertoire) # "/Users/enzojuzyna/Downloads/projet/speeches"    
+    directory = './{}'.format(repertoire)  # "/Users/enzojuzyna/Downloads/projet/speeches"
     files_names = list_of_files(directory, "txt")
-#on va créer une liste qui va contenir tous les mots présents dans les textes du répertoire en un seul exemplaire
+    # on va créer une liste qui va contenir tous les mots présents dans les textes du répertoire en un seul exemplaire
     contenu = ""
     liste_contenu = []
     for i in files_names:
-        with open(directory + '/' + i, 'r') as fichier_IDF :
+        with open(directory + '/' + i, 'r') as fichier_IDF:
             contenu = fichier_IDF.read()
             contenu = contenu.split()
             liste_contenu += contenu
     set_mot = set(liste_contenu)
-#On crée un dico qui va contenir tous les mots ainsi que leurs IDF
+    # On crée un dico qui va contenir tous les mots ainsi que leurs IDF
     dico = {}
-#Pour chaque mot on va vérifier s'il se trouve dans un document et pour chaque document on va ajouté +1 à un compteur 
-#afin de pourvoir calculer l'IDF
+    # Pour chaque mot on va vérifier s'il se trouve dans un document et pour chaque document on va ajouté +1 à un compteur
+    # afin de pourvoir calculer l'IDF
     for mot in set_mot:
         occurence = 0
         for i in files_names:
-            with open(directory + '/' + i, 'r') as fichier_IDF :
+            with open(directory + '/' + i, 'r') as fichier_IDF:
                 contenu = fichier_IDF.read()
                 if mot in contenu:
                     occurence += 1
@@ -169,26 +183,28 @@ def IDF_par_fichier(repertoire):
         dico[mot] = calcul_idf
     return dico
 
+
 a = IDF_par_fichier("cleaned")
 
-#Fonction TF_IDF 
+
+# Fonction TF_IDF
 
 def TF_IDF(repertoire):
-    # Initialisation d'un dictionnaire pour stocker la matrice TF-IDF, d'un dico pour recevoir la valeur de chaque mot et son idf, 
+    # Initialisation d'un dictionnaire pour stocker la matrice TF-IDF, d'un dico pour recevoir la valeur de chaque mot et son idf,
     # ansi la récuparation du chemin et de la liste des noms du fichier
     matrice_TF_IDF = {}
     dico_IDF = IDF_par_fichier(repertoire)
-    directory = './{}'.format(repertoire)   
+    directory = './{}'.format(repertoire)
     files_names = list_of_files(directory, "txt")
 
-    # On fait une boucle sur chaque mot du dictionnaire 
+    # On fait une boucle sur chaque mot du dictionnaire
     for mot in list(dico_IDF.keys()):
         resultat_TF_IDF = []
 
         # on fait une seconde boulce sur le nom des fichiers
         for i in files_names:
-            
-            # Ouverture du fichier en mode lecture 
+
+            # Ouverture du fichier en mode lecture
             with open("./cleaned/{}".format(i), "r") as fichier1:
 
                 # On cherche à obtenir une liste de tous les mots
@@ -196,7 +212,7 @@ def TF_IDF(repertoire):
                 contenu_mot = contenu.split()
 
             valeur_TF = 0
-            # Vérification si le mot est présent dans le contenu du fichier, pout optimiser, gagner en vitesse, si le mot n'est pas 
+            # Vérification si le mot est présent dans le contenu du fichier, pout optimiser, gagner en vitesse, si le mot n'est pas
             # dans la liste, on ne cherche pas à chercher à l'occurence
             if (mot in contenu_mot) == True:
                 # Fonction TF nn optimisée trop longue, changement de méthode
@@ -205,9 +221,9 @@ def TF_IDF(repertoire):
                 for i in contenu_mot:
                     if mot == i:
                         valeur_TF += 1
-           
-            # Si mot pas dans le contenue, alors son TF = 0           
-            else : 
+
+            # Si mot pas dans le contenue, alors son TF = 0
+            else:
                 valeur_TF = 0
 
             # On calcul TF_IDF, en multipliant la valeur TF par son IDF
@@ -218,6 +234,7 @@ def TF_IDF(repertoire):
     # print(len(matrice_TF_IDF))
     return matrice_TF_IDF
 
+
 # On teste la fonction avec le répertoire cleaned.
 res = TF_IDF("cleaned")
 print(res)
@@ -225,37 +242,38 @@ print(res)
 ###############################################
 ### Question 1 Affichage valeur TD-IDF == 0 ###
 ###############################################
-
+def TF_IDF_NUL():
 # Appel de la fonction TF_IDF pour obtenir la matrice
-valeur_td_idf = TF_IDF("cleaned")
-L = []
+    valeur_td_idf = TF_IDF("cleaned")
+    L = []
 
-# On fait une boucle où i va aller piocher des tuples des cles et des valeurs de la liste 
-for i in valeur_td_idf.items():
-    occurence = 0
-    # On va ensuite compter le nombre de 0, pour savoir si ce mot à un TD-IDF = 0 dans tous les fichiers
-    for zero in i:
-        if zero == 0:
-            occurence += 1
-        else:
-            break
-        # si on a des valeurs TD-IDF dans tous les fichiers pour ce mot, il est alors ajouter à la liste.
-        if occurence == 8:
-            L.append(i[0])
-print(L)
-
+    # On fait une boucle où i va aller piocher des tuples des cles et des valeurs de la liste
+    for i in valeur_td_idf.items():
+        occurence = 0
+        # On va ensuite compter le nombre de 0, pour savoir si ce mot à un TD-IDF = 0 dans tous les fichiers
+        for zero in i:
+            if zero == 0:
+                occurence += 1
+            else:
+                break
+            # si on a des valeurs TD-IDF dans tous les fichiers pour ce mot, il est alors ajouter à la liste.
+            if occurence == 8:
+                L.append(i[0])
+    print(L)
+    return
 #####################################################
 ### Question 2  mot avec le TD-IDF le plus élevé ###
 ####################################################
 
-Liste_tf_idf_eleve = []
-for i in res.items():
-    nb = 0
-    for values in i[1]:
-        nb += values
-    Liste_tf_idf_eleve.append(i[0])
-print("le(s) mot(s) ayant le score TF-IDF le plus élevé sont ", min(Liste_tf_idf_eleve))
-
+def TF_IDF_HIGH():
+    Liste_tf_idf_eleve = []
+    for i in res.items():
+        nb = 0
+        for values in i[1]:
+            nb += values
+        Liste_tf_idf_eleve.append(i[0])
+    print("le(s) mot(s) ayant le score TF-IDF le plus élevé sont ", min(Liste_tf_idf_eleve))
+    return
 
 #############################################################################
 ### Question 3   Indiquer le mot le plus utilisé par le predident chirac ###
@@ -263,23 +281,22 @@ print("le(s) mot(s) ayant le score TF-IDF le plus élevé sont ", min(Liste_tf_i
 
 # On crée une fonction pour trouver le(s) mot(s) le(s) plus répété(s) par le président Chirac
 def indiquer_le_mot_plus_utilise(president):
-
     # On donne le chemin pour acceder au fichier qui contiennent le nom du president recherche
-    directory = './cleaned'  
+    directory = './cleaned'
     files_names = list_of_files(directory, "txt")
-    
+
     # On créera un liste max qui prendra pour valeur le mot avec le plus d'occurences, on l'initialise à 0, 0. car on mettra en premier
     # le mot puis son occurence
     max = [0, 0]
     contenu = ""
-    
-    # On fait une boucle des fichiers pour obtenir le contenu (chaine de caractère) de tous les fichiers réunis qui correspondent 
+
+    # On fait une boucle des fichiers pour obtenir le contenu (chaine de caractère) de tous les fichiers réunis qui correspondent
     # à ceux dont l'auteur est Chirac.
     for i in files_names:
         if president in i:
-            with open (directory + '/'+ i, 'r') as fichier:
+            with open(directory + '/' + i, 'r') as fichier:
                 contenu += fichier.read() + " "
-    
+
     # On utilise la fonction TF qui correspond à occurences_chaines(chaine) pour avoir un dico de chaque mot et de leur occurence
     dico = occurences_chaines(contenu)
     for a in dico.items():
@@ -287,7 +304,7 @@ def indiquer_le_mot_plus_utilise(president):
         if max[1] < a[1] and a[0] != '':
             max = a
 
-    # on verfie si il y a plusieurs mots qui ont la même occurence que max, si oui on les ajoutera dans la liste 
+    # on verfie si il y a plusieurs mots qui ont la même occurence que max, si oui on les ajoutera dans la liste
     liste_mot = [max]
     for j in dico.items():
         if max[1] == j[1]:
@@ -295,36 +312,37 @@ def indiquer_le_mot_plus_utilise(president):
                 liste_mot.append(j[0])
     return liste_mot
 
-print(indiquer_le_mot_plus_utilise("Chirac"))
 
+print(indiquer_le_mot_plus_utilise("Chirac"))
 
 ################################################################################
 ### 5. Indiquer le premier président à parler du climat et/ou de l’écologie ###
 ###############################################################################
 
-listes_president = []
-dico_climat_eco = {}
+def president_ecologie():
+    listes_president = []
+    dico_climat_eco = {}
 
-for i in list_of_files("cleaned", "txt"):
-    with open("./cleaned/{}".format(i), 'r') as fichier:
-        contenu = fichier.read()
-        texte_mot_cleaned = contenu.split()
-        nom_president = (i[11:-4])
-        for index in range(len(texte_mot_cleaned)):
-            if texte_mot_cleaned[index] == "climat" or texte_mot_cleaned[index] == "écologie":
-                if not nom_president in dico_climat_eco.keys():
-                    dico_climat_eco[nom_president] = index
+    for i in list_of_files("cleaned", "txt"):
+        with open("./cleaned/{}".format(i), 'r') as fichier:
+            contenu = fichier.read()
+            texte_mot_cleaned = contenu.split()
+            nom_president = (i[11:-4])
+            for index in range(len(texte_mot_cleaned)):
+                if texte_mot_cleaned[index] == "climat" or texte_mot_cleaned[index] == "écologie":
+                    if not nom_president in dico_climat_eco.keys():
+                        dico_climat_eco[nom_president] = index
 
-min_key = min(dico_climat_eco, key=dico_climat_eco.get)
-print("Le premier président à parler du climat et/ou de l’écologie est", min_key)
-
+    min_key = min(dico_climat_eco, key=dico_climat_eco.get)
+    print("Le premier président à parler du climat et/ou de l’écologie est", min_key)
+    return
 #############################################################
 ### 6. le(s) mot(s) que tous les présidents ont évoqués. ###
 ############################################################
-
-mots_evoque = []
-for score in res:
-    if min(score) != 0:
-        mots_evoque.append(score)
-print("le(s) mot(s) que tous les présidents ont évoqués sont:",mots_evoque)
-
+def mot_globaux_presidents():
+    mots_evoque = []
+    for score in res:
+        if min(score) != 0:
+            mots_evoque.append(score)
+    print("le(s) mot(s) que tous les présidents ont évoqués sont:", mots_evoque)
+    return
