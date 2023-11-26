b1534ba2905d54636ba5e2c38172a8032111d872
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2023-11-26 14:43:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -476,9 +476,13 @@ def mot_evoque():
 
     # On fait une boucle sur les mots de la matrice TF_IDF
     # Si le mots a un score minimum dans tous les discours est différent de 0 alors on l'ajoute à la liste
-    for score in res:
-        if min(res[score]) != 0:
-            mots_evoque.append(score)
+    for score in res.items():
+        min = 99999999
+        for i in score[1]:
+            if min > i:
+                min = i
+        if min != 0:
+            mots_evoque.append(score[0])
     # on revoit le(s) mot(s) que tous les présidents ont évoqués
     return ("le(s) mot(s) que tous les présidents ont évoqués sont:", mots_evoque)
 
