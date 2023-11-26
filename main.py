69e2c0b534d48450d220145366bd312e7356d5a2
Enzo-JZN <enzo@juzyna.com>
2023-11-26 13:13:27 +0100

    fonction6

--- main.py
+++ main.py
@@ -465,3 +465,24 @@ def climat_president():
 
 # Appelle la fonction et imprime le résultat
 print(climat_president())
+
+#############################################################
+### 6. le(s) mot(s) que tous les présidents ont évoqués. ###
+############################################################
+
+def mot_evoque():
+    # On crée une boucle pour stocker les mots dit par tous les présidents
+    mots_evoque = []
+
+    # On fait une boucle sur les mots de la matrice TF_IDF
+    # Si le mots a un score minimum dans tous les discours est différent de 0 alors on l'ajoute à la liste
+    for score in res:
+        if min(res[score]) != 0:
+            mots_evoque.append(score)
+    # on revoit le(s) mot(s) que tous les présidents ont évoqués
+    return ("le(s) mot(s) que tous les présidents ont évoqués sont:", mots_evoque)
+
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
