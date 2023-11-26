7965141208d31be330dc0b26bb9e6c6acb44cb84
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-11-26 16:05:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,11 +76,10 @@ def convertir_textes_miniscules():
     for i in files_names:
         # On crée le chemin et le noveau nom du fichier copier
         chemin_cleaned = directory_cleaned + "/" + i
-        chemin_cleaned = directory_cleaned + "/" + i
 
-        # On crée le fihcier copier, puis on copie le contenu du premier fihcier et on le rajoute le tout en miniscule
-        with open(chemin_cleaned, "w") as fichier:
-            fichier2 = open(directory + "/" + i, "r")
+        # On crée le fichier copier, puis on copie le contenu du premier fichier et on le rajoute le tout en minuscule
+        with open(chemin_cleaned, "w", encoding="utf-8") as fichier:
+            fichier2 = open(directory + "/" + i, "r", encoding="utf-8")
             contenu = fichier2.read()
             fichier.write(contenu.lower())
 
