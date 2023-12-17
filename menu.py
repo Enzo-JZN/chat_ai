da3702be44527f65d858887c93202f11a7c3a593
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-12-17 18:36:38 +0100

    Update menu.py
    traduction variables en français

--- menu.py
+++ menu.py
@@ -16,36 +16,36 @@ def menu():
           "- Tapez 4 : Si vous voulez avoir le nom des présidents qui ont parlé de la Nation et celui l'ayant fait le plus souvent.\n"
           "- Tapez 5 : Si vous voulez savoir qui est le premier président à avoir parlé de l'écologie.\n"
           "- Tapez Q : Si vous souhaitez quitter le menu.")
-    choice = input("Tapez : ")
+    choix = input("Tapez : ")
 
     clear_terminal()
 
     # Selon le choix de l'utilisateur on appelle la fonction correspondante
-    if choice == "1":
+    if choix == "1":
         print(calcul_mot_dit_nn_important(), mot_moins_important())
-    elif choice == "2":
+    elif choix == "2":
         print(score_IDF_eleve())
-    elif choice == "3":
+    elif choix == "3":
         print(indiquer_le_mot_plus_utilise("Chirac"))
-    elif choice == "4":
+    elif choix == "4":
         print(president_nation())
-    elif choice == "5":
+    elif choix == "5":
         print(climat_president())
-    elif choice.upper() == "Q":
+    elif choix.upper() == "Q":
         print("Au revoir.")
         return
 
     # On met une sécurité si jamais l'utilisateur s'est trompé de commande et lui propose de recommencer
     else:
-        retry = input("Ce n'est pas une possibilité, voulez-vous réessayer :\n"
+        essai2 = input("Ce n'est pas une possibilité, voulez-vous réessayer :\n"
                       "- Tapez 1 pour Oui\n"
                       "- Tapez 2 pour Non\n")
-        if retry == "1":
+        if essai2 == "1":
             menu()
         else:
             print("Au revoir.")
 
-    if choice != 'Q':
+    if choix != 'Q':
         # On propose à l'utilisateur d'essayer une autre possibilité sauf s'il voulait quitter
         encore = input("Voulez-vous essayer une autre possibilité prédéfini ?\n"
                        "- Tapez 1 pour Oui\n"
