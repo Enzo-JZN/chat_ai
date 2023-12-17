a73f6d96a2854847fec71c2bf146a2f2296ab70f
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-12-17 11:00:21 +0100

    Create menu_chatbot.py

---
+++ menu_chatbot.py
@@ -0,0 +1,7 @@
+from questions_part2 import *
+
+def menu_chatbot():
+    print('Bienvenue dans ce Chatbot :')
+    print("Ce Chatbot va répondre à vos questions en analysant des textes de présidents français.")
+    question = input("Quelle est votre question ?")
+#    print(generer_reponse(question, 'cleaned', document, ))
