6ae471a7d2f4ed50c00d2a7412734a7e20e35c8b
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-12-17 11:10:29 +0100

    Update menu_chatbot.py

--- menu_chatbot.py
+++ menu_chatbot.py
@@ -1,7 +1,16 @@
 from questions_part2 import *
-
+def reponses_chatbot():
+    question = input("Quelle est votre question ?")
+    #print(generer_reponse(question, 'cleaned', document, ))
+    
 def menu_chatbot():
     print('Bienvenue dans ce Chatbot :')
     print("Ce Chatbot va répondre à vos questions en analysant des textes de présidents français.")
-    question = input("Quelle est votre question ?")
-#    print(generer_reponse(question, 'cleaned', document, ))
+    print("Voulez-vous poser une autre question ?")
+    print("Si oui : Tapez 1")
+    print("Si non : Tapez 2")
+    reponse = input("Que voulez-vous :")
+    if reponse == '1':
+        reponses_chatbot()
+    else:
+        print("Au revoir.")
