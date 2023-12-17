6103c2632b22a18fc83a4d620de33c0548b7b8f9
Wooshayl <151762308+Wooshayl@users.noreply.github.com>
2023-12-17 18:32:57 +0100

    Update menu_chatbot.py

--- menu_chatbot.py
+++ menu_chatbot.py
@@ -1,12 +1,19 @@
-from questions_part2 import *
+from menu import *
 def reponses_chatbot():
-    question = input("Quelle est votre question ?")
-    #print(generer_reponse(question, 'cleaned', document, ))
-    
+    chaine = input("Posez une question : ")
+    print(affiner_reponse(chaine, trouver_occurrence_et_phrase(calcul_doc_plus_pertinent(chaine),
+                                                               trouver_mot_plus_important_TF_IDF(chaine))))
+    autre_quest = input(("Voulez vous poser une autre question ? \n"
+                         "Si oui : Tapez 1\n"
+                         "Si non : Tapez 2\n"))
+    if autre_quest == '1':
+        reponses_chatbot()
+    else:
+        print("Au revoir")
 def menu_chatbot():
     print('Bienvenue dans ce Chatbot :')
     print("Ce Chatbot va répondre à vos questions en analysant des textes de présidents français.")
-    print("Voulez-vous poser une autre question ?")
+    print("Voulez-vous poser une question ?")
     print("Si oui : Tapez 1")
     print("Si non : Tapez 2")
     reponse = input("Que voulez-vous :")
