f2d949bed83d30fada9cd1d6dd7fb3285804deed
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2023-12-19 00:01:33 +0100

    Update questions_part1.py

--- questions_part1.py
+++ questions_part1.py
@@ -18,16 +18,11 @@ def calcul_mot_dit_nn_important():
 
     # On fait une boucle où i va aller piocher des tuples des cles et des valeurs de la liste
     for i in valeur_tf_idf.items():
-        occurence = 0
-        # On va ensuite compter le nombre de 0, pour savoir si ce mot à un TD-IDF = 0 dans tous les fichiers
-        for zero in i[1]:
-            if int(zero) == 0:
-                occurence += 1
-            else:
-                break
-            # si on a des valeurs TD-IDF dans tous les fichiers pour ce mot, il est alors ajouter à la liste.
-            if occurence == 8:
-                L.append(i[0])
+        addition = 0
+        for somme in i[1]:
+                addition += somme
+        if addition == 0:
+            L.append(i[0])
     return (L)
 
 
